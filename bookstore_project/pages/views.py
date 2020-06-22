03e5f91178db9cfb000926cccc84e8689133fbf9
Barun <bhattacharjeebarun25@gmail.com>
2020-06-22 21:36:00 +0600

    ch4

---
+++ bookstore_project/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+'''
+We can leverage Django’s built-in TemplateView so
+that the only tweak needed is to specify our desired template, home.html
+'''
+class HomePageView(TemplateView):
+    template_name = 'home.html'

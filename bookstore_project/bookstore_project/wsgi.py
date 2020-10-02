13ccda0205dd5e4932f37084ea6234e771b80cd5
Barun <bhattacharjeebarun25@gmail.com>
2020-10-02 12:36:39 +0600

    ch18-c11

--- bookstore_project/bookstore_project/wsgi.py
+++ bookstore_project/bookstore_project/wsgi.py
@@ -9,15 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-import sys
-
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('<bookstore_project>/')
-sys.path.append('<bookstore_project>/users')
-#sys.path.append('<bookstore_project>/pages')
-#sys.path.append('<bookstore_project>/orders')
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'bookstore_project.bookstore_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore_project.settings')
 
 application = get_wsgi_application()
